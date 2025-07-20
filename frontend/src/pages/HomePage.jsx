84fe520fc53c9d758e99e07f6b821f0eef7bfe1e
BasilBosnjak <basil.bosnjak@gmail.com>
2025-07-20 19:16:19 +0200

    Update and delete on the frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
 import api from '../lib/axios'
 import toast from 'react-hot-toast'
 import NoteCard from '../components/NoteCard'
+import NotesNotFound from '../components/NotesNotFound'
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false)
@@ -30,10 +31,10 @@ const HomePage = () => {
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     fetchNotes();
-  },[])
+  },[]);
   return (
     <div className='min-h-screen'>
       <Navbar />
@@ -43,16 +44,18 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading &&  <div className="text-center text-primary py-10"> Loading notes...</div>}
 
+        {!loading && notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {notes.map(note => (
-              <NoteCard key={note._id} note={note} />
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage
